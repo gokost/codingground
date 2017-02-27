5fd27569e4f5d60ef2153633eac6d6878aa5eeb1
gokost <rifalfahrudin@gmail.com>
2017-02-27 08:36:39 +0700

    Created by Coding Ground Client

---
+++ Pretest pemahaman java/Tugas1.java
@@ -0,0 +1,11 @@
+public class Tugas1{
+   
+     
+     public static void main(String []args){
+        String nama = "Ahmad Rifal Fahrudin";
+        String kota = "Jepara";
+        
+        System.out.println("Nama      :"+nama);
+        System.out.println("kota Asal :"+kota);
+     }
+}
