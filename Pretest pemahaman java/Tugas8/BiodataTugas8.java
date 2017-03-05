5a5313bf46b97e85e40331b3086073b76eba8faf
rifalfahrudin <rifalfahrudin@gmail.com>
2017-03-05 05:32:13 -0800

    Created by Coding Ground Client

---
+++ Pretest pemahaman java/Tugas8/BiodataTugas8.java
@@ -0,0 +1,9 @@
+public class BiodataTugas8{
+	String nama = "Ahmad Rifal Fahrudin";
+	String alamat = "Ngabul";
+	int umur = 20;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
