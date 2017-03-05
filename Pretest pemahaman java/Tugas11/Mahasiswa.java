d7e91440b23fca3e47f6d010ba06a1762e12833d
rifalfahrudin <rifalfahrudin@gmail.com>
2017-03-05 21:02:47 +0700

    Created by Coding Ground Client

---
+++ Pretest pemahaman java/Tugas11/Mahasiswa.java
@@ -0,0 +1,8 @@
+public class Mahasiswa extends BiodataTugas11{
+	String nim = "141240000241";
+	String jurusan = "Teknik Informatika";
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur + "\nNIM : " + nim + "\nJurusan : " + jurusan);
+	}
+}
